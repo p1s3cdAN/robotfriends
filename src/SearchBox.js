a7dd5e653e725916aec0d23747ab6b33a57763b7
p1s3cdAN <pisecdan@yahoo.com>
2019-10-15 20:13:54 +0300

    added event listener and state to seachbox

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import CardList from './CardList';
 
-const SearchBox = () =>{
+const SearchBox = ({searchChange}) =>{
     return(
         <div className = 'pa2'>
             <input 
                 className= 'pa3 ba b--green bg-lightest-blue'
                 type='search' 
                 placeholder='search robots'
+                onChange={searchChange}
             />
         </div>
     );
