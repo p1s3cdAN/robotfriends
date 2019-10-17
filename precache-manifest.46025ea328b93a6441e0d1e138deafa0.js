59931cdaad81cda517134c62f612933879f8a937
p1s3cdAN <pisecdan@yahoo.com>
2019-10-17 15:06:38 +0300

    Updates

---
+++ precache-manifest.46025ea328b93a6441e0d1e138deafa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff820656b27205a56347e95e4b8ceb62",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "cf2b61a9b5d0085899cd",
+    "url": "/robofriends/static/css/main.bd98e30f.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/robofriends/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "cf2b61a9b5d0085899cd",
+    "url": "/robofriends/static/js/main.84c2ecdc.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
